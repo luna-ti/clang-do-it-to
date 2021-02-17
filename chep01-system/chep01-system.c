fd0396779d22b6966819e79dc771566ebf68a35a
luna-ti <jmh4473@naver.com>
2021-02-17 15:03:51 +0900

    update

---
+++ chep01-system/chep01-system.c
@@ -0,0 +1,33 @@
+﻿// chep01-system.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+/*
+* chap01-sysinfo
+* 시스템 정보 확인
+* -------------------------
+* sizeof(type) 내장함수(Built=in Function)
+*/
+#include <stdio.h>
+
+int main()
+{
+    printf(">>> ststem information <<<\n");
+
+    printf("sizeof(char)(%d)\n", sizeof(char));
+    printf("sizeof(short)(%d)\n", sizeof(short));
+    printf("sizeof(int)(%d)\n", sizeof(int));
+    printf("sizeof(long)(%d)\n", sizeof(long));
+    printf("sizeof(float)(%d)\n", sizeof(float));
+    printf("sizeof(double)(%d)\n", sizeof(double));
+
+    printf("sizeof(char)(%d)\n", sizeof(char));
+    printf("sizeof(short)(%d)\n", sizeof(short));
+    printf("sizeof(int)(%d)\n", sizeof(int));
+    printf("sizeof(long)(%d)\n", sizeof(long));
+    printf("sizeof(float)(%d)\n", sizeof(float));
+    printf("sizeof(double)(%d)\n", sizeof(double));
+
+    printf("sizeof(void)(%d)\n", sizeof(void));
+}
+ 
+
+
